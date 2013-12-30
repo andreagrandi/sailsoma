be80b1f4757d7c3c8227d551b4596ee638eb1b55
Andrea Grandi <a.grandi@gmail.com>
2013-12-30 16:01:41 +0100

    Connect the C++ classes with QML

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 
 #include <QGuiApplication>
 #include <QQuickView>
+#include <QQmlContext>
 
 #include "sailfishapplication.h"
+#include "servercomm.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QGuiApplication> app(Sailfish::createApplication(argc, argv));
     QScopedPointer<QQuickView> view(Sailfish::createView("main.qml"));
+
+    ServerComm sc;
+    view->rootContext()->setContextProperty("serverComm", &sc);
     
     Sailfish::showView(view.data());
     
