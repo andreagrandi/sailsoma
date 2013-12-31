7acf0026a4cfb7ecdd0f4c4988a2ed39207d9601
Andrea Grandi <a.grandi@gmail.com>
2013-12-31 15:16:21 +0100

    Add trasparency support for the app cover

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <QGuiApplication>
 #include <QQuickView>
 #include <QQmlContext>
+#include <QQuickWindow>
 
 #include "sailfishapplication.h"
 #include "servercomm.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+    QQuickWindow::setDefaultAlphaBuffer(true);
     QScopedPointer<QGuiApplication> app(Sailfish::createApplication(argc, argv));
     QScopedPointer<QQuickView> view(Sailfish::createView("main.qml"));
 
