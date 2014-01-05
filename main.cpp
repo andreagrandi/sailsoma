6a0753d405ee41dd27476e0c62babc5be12d5050
Andrea Grandi <a.grandi@gmail.com>
2014-01-05 11:32:46 +0100

    More fixes to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QtQuick>
 #endif
 
+#include <QGuiApplication>
 #include <QQuickView>
 #include <QQmlContext>
 #include <sailfishapp.h>
@@ -9,9 +10,11 @@
 
 int main(int argc, char *argv[])
 {
+    QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QScopedPointer<QQuickView> view(SailfishApp::createView());
     ServerComm sc;
     view->rootContext()->setContextProperty("serverComm", &sc);
     view->setSource(SailfishApp::pathTo("main.qml"));
-    view->show();
+    view->showFullScreen();
+    return app->exec();
 }
